1063bc13eec8caa01e697ca876588b95436a659e
csmhowitzer <csmhowitzer@gmail.com>
2025-03-19 13:48:29 -0400

    working on server streaming

--- GrpcService.Client/Program.cs
+++ GrpcService.Client/Program.cs
@@ -6,23 +6,35 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // using var channel = GrpcChannel.ForAddress("https://localhost:7116");
-        // var client = new Greeter.GreeterClient(channel);
-        // var reply = await client.SayHelloAsync(new HelloRequest { Name = "Wicked Awesome Client" });
-        // Console.WriteLine($"Greeting: {reply.Message}");
-
-        // var wClient = new WeatherService.WeatherServiceClient(channel);
-        // var wReply = await wClient.GetCurrentWeatherAsync(
-        //     new GetCurrentWeatherForCityRequest
-        //     {
-        //         City = "London",
-        //         Units = GetCurrentWeatherForCityRequest.Types.Units.Metric,
-        //     }
-        // );
-        // Console.WriteLine($"Temp: {wReply.Temperature}\r\nFL: {wReply.FeelsLike}");
-        // Console.WriteLine(wReply);
-
         using var channel = GrpcChannel.ForAddress("https://localhost:7116");
+        await StreamCurrentWeatherRequest(channel);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    public static async Task UniaryCall(GrpcChannel channel)
+    {
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "Wicked Awesome Client" });
+        Console.WriteLine($"Greeting: {reply.Message}");
+    }
+
+    public static async Task RequestCurrentWeather(GrpcChannel channel)
+    {
+        var wClient = new WeatherService.WeatherServiceClient(channel);
+        var wReply = await wClient.GetCurrentWeatherAsync(
+            new GetCurrentWeatherForCityRequest
+            {
+                City = "London",
+                Units = GetCurrentWeatherForCityRequest.Types.Units.Metric,
+            }
+        );
+        Console.WriteLine($"Temp: {wReply.Temperature}\r\nFL: {wReply.FeelsLike}");
+        Console.WriteLine(wReply);
+    }
+
+    public static async Task StreamCurrentWeatherRequest(GrpcChannel channel)
+    {
         var sClient = new WeatherService.WeatherServiceClient(channel);
         using var call = sClient.GetCurrentWeatherStream(
             new GetCurrentWeatherForCityRequest
@@ -31,13 +43,9 @@ class Program
                 Units = GetCurrentWeatherForCityRequest.Types.Units.Imperial,
             }
         );
-
-        while (await call.ResponseStream.MoveNext(default))
+        while (await call.ResponseStream.MoveNext(new CancellationToken()))
         {
             Console.WriteLine($"From Server: {call.ResponseStream.Current}");
         }
-
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
     }
 }
